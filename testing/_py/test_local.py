5e3568ec58f65619e4b821c822b01b4942af9296
shonilbhide <bhideshonil@gmail.com>
2025-04-21 16:33:28 -0400

    test8

--- testing/_py/test_local.py
+++ testing/_py/test_local.py
@@ -27,13 +27,11 @@ class CommonFSTests:
     def test_constructor_equality(self, path1):
         p = path1.__class__(path1)
         assert p == path1
-        assert False, "Intentional failure for CI testing"
 
     def test_eq_nonstring(self, path1):
         p1 = path1.join("sampledir")
         p2 = path1.join("sampledir")
         assert p1 == p2
-        assert False, "Intentional failure for CI testing"
 
 
     def test_new_identical(self, path1):
